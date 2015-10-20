701d894030ff2090ac22bc46c19773f7e592e997
Alvaro Mangual <amangual@live.com>
2015-10-20 13:53:29 -0500

    Checkpoint 21, looking for fix to address_book_spec.rb failures

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,9 +1,7 @@
 require_relative '../models/entry.rb'
 
 RSpec.describe Entry do
-  
   context "attributes" do
-  
     it "should respond to name" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:name)
@@ -24,7 +22,6 @@ RSpec.describe Entry do
     it "prints an entry as a string" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
-
       expect(entry.to_s).to eq(expected_string)
     end
   end
